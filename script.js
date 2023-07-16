d875b8ff0cee738c2a213463428bf720ededffd6
Jens gerwins <Jens.gerwins@gmail.com>
2023-07-16 14:38:53 +0200

    Frontend bereich

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const cardWapper = document.getElementById("pokemon-card-wapper");
 const cardimg1 = document.getElementById("PokemonCard1");
+const cardimg2 = document.getElementById("PokemonCard2");
+const cardimg3 = document.getElementById("PokemonCard3");
+const cardimg4 = document.getElementById("PokemonCard4");
+const cardimg5 = document.getElementById("PokemonCard5");
+
 
 
 
@@ -23,21 +28,34 @@ function loadFetch() {
 loadFetch();
 function render() {
 
+
+
+
     for (let i = 0; i < Pokemons.length; i++) {
         const object = Pokemons[i];
-        console.log(object.img)
+
+
 
 
 
 
         if (object.name === "Bisasam") {
             cardimg1.src = object.img;
+            cardimg2.src = object.img;
+            cardimg3.src = object.img;
+            cardimg4.src = object.img;
+            cardimg5.src = object.img;
 
-            document.cardWapper.append(cardimg1);
+            document.cardWapper.append(cardimg1, cardimg2, cardimg3, cardimg4, cardimg5);
 
         }
         else {
             console.log("fehler")
         }
     }
+}
+console.log(arr)
+function setImg() {
+    cardimg1.src = object.img;
+    document.cardWapper.append(cardimg1);
 }
